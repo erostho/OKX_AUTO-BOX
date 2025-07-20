57dc71892161c62c021e1a9f68b317c197d60975
erostho <erostho1202@gmail.com>
2025-07-20 14:40:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ def run_bot():
                         logging.info(f"✅ Đặt TP/SL với entry={entry_price}, size={pos_size}")
                         break
                 else:
-                logging.error(f"⚠️ không tìm được entry_price hợp lệ để đặt TP/SL cho {symbol}")
+                    logging.error(f"⚠️ không tìm được entry_price hợp lệ để đặt TP/SL cho {symbol}")
                 
             
                 # ✅ Tính TP/SL
